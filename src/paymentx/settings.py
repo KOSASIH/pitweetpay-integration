4c8b4e1b92137ca8abbc8ba438af46bf3419b892
KOSASIH <kosasihg88@gmail.com>
2024-05-24 20:09:16 +0700

    Create settings.py

---
+++ src/paymentx/settings.py
@@ -0,0 +1,4 @@
+PAYMENTX_API_KEY = "your_paymentx_api_key"
+PAYMENTX_API_SECRET_KEY = "your_paymentx_api_secret_key"
+PI_NETWORK_API_KEY = "your_pi_network_api_key"
+PI_NETWORK_REDIRECT_URL = "https://your-domain.com/pi_network/callback/"
